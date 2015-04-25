597abfa7e8eed54b3c2493d6744c4a701edacdee
Tomasz Kowalczyk <tomek@kownet.info>
2015-04-25 11:17:34 +0200

    menu icons

--- Application/Droid/Resources/Resource.designer.cs
+++ Application/Droid/Resources/Resource.designer.cs
@@ -47,7 +47,19 @@ namespace DevTalkMobile.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int icon = 2130837504;
+			public const int about = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int chat = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int home = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int icon = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int podcast = 2130837508;
 			
 			static Drawable()
 			{
